ec81210b236184c9f0f1cd1674f49642568e3f47
Chris Gibson <chris.david.gibson@gmail.com>
2014-12-01 21:02:19 -0600

    Adding public html stuff

---
+++ src/server/grinding.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, '../public/media')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../public', 'index.html'))
+})
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('__dirname', __dirname)
+  console.log('Listening at http://%s:%s', host, port)
+
+})
